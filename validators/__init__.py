43a5676722b5d3d360890dbfd4e694749504dda8
gsong <george@55minutes.com>
2009-04-15 03:54:40 +0000

    Luhn credit card number validator.
    git-svn-id: file:///opt/custom/var/svn/repos/internal/fiftyfive/python/trunk/lib@8549 07aa3797-eaea-0310-9497-ec20f46c85ba

---
+++ validators/__init__.py
@@ -0,0 +1,1 @@
+from credit_card_number import *
---
+++ validators/credit_card_number.py
@@ -0,0 +1,36 @@
+from fiftyfive.metaclass import PluginMount
+
+__all__ = ('CreditCardValidators',)
+
+class CreditCardValidationError(Exception):
+    pass
+
+class CreditCardValidators(object):
+    """
+    Plugins extending this class will be used to verify credit card
+    numbers.
+
+    Valid plugins must provide the following **cls** method:
+
+    verify(cc_number):
+        Either finishes silently or raises a CreditCardValidationError error.
+    """
+    __metaclass__ = PluginMount
+
+    error = CreditCardValidationError
+
+class Luhn(CreditCardValidators):
+    "See http://en.wikipedia.org/wiki/Luhn_algorithm for more info."
+    def verify(cls, ccn):
+        digits = [int(i) for i in list(str(ccn))]
+        _sum = 0
+        alt = False
+        for d in reversed(digits):
+            if alt:
+                d *= 2
+                if d > 9:
+                    d -= 9
+            _sum += d
+            alt = not alt
+        if not (_sum % 10) == 0:
+            raise cls.error("Invalid credit card number")
