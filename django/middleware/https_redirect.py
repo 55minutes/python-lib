e2432f2641838a04300e65beba46eeba5e75a7a3
gsong <george@55minutes.com>
2009-04-16 22:08:42 +0000

    Added middleware documentation.
    git-svn-id: file:///opt/custom/var/svn/repos/internal/fiftyfive/python/trunk/lib@8560 07aa3797-eaea-0310-9497-ec20f46c85ba

--- django/middleware/https_redirect.py
+++ django/middleware/https_redirect.py
@@ -8,6 +8,22 @@ __all__ = ('HttpsRedirect',)
 SSL = 'SSL'
 
 class HttpsRedirect(object):
+    """
+    Django middleware to redirect ``http`` and ``https`` traffic.
+
+    The middleware uses the fact that in URLconfs you can pass additional
+    view parameters directly at the url pattern definition. For URLs that
+    require SSL, you can mark them as such::
+
+        url(regex, view, {'SSL':True}, name=url_name)
+
+    For URLs marked with ``SSL=True``, the middleware will force a redirect
+    to ``https`` protocol. For URLs marked with ``SSL=False`` or not explicitly
+    marked, the middleware will force a redirect to ``http`` protocol.
+
+    The ``SSL`` keyword argument is stripped from the view keyword arguments
+    before passing on to the next middleware or to the view.
+    """
     def process_view(self, request, view_func, view_args, view_kwargs):
         secure = view_kwargs.pop(SSL, False)
         if secure != self.is_secure(request) and self.has_ssl():
