794cd5932af1744b7748b26532232755945526be
gsong <george@55minutes.com>
2009-04-25 06:06:00 +0000

    Applied Apache 2.0 license.
    git-svn-id: file:///opt/custom/var/svn/repos/internal/fiftyfive/python/trunk/lib@8606 07aa3797-eaea-0310-9497-ec20f46c85ba

--- utils/module_tools/module_loader.py
+++ utils/module_tools/module_loader.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2009 55 Minutes (http://www.55minutes.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import imp, sys, types
 
 __all__ = ('find_or_load_module',)
@@ -60,3 +76,4 @@ def find_or_load_module(module_name, path=None):
     else:
         raise ImportError("Don't know how to handle this module type.")
     return module
+
--- utils/testing/coverage_report/html_module_detail.py
+++ utils/testing/coverage_report/html_module_detail.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2009 55 Minutes (http://www.55minutes.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import cgi, os
 
 from data_storage import ModuleVars
@@ -72,3 +88,4 @@ def html_module_detail(filename, module_name, nav=None):
         print >>fo, nav_html
     print >>fo, module_detail.BOTTOM
     fo.close()
+
