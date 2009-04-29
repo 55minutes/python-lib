9279e21c271eb32c1049c3e42b5d5b39982adc87
ravasthi <ravasthi@55minutes.com>
2009-04-29 22:35:31 +0000

    Back out change, since django.utils has this already.
    git-svn-id: file:///opt/custom/var/svn/repos/internal/fiftyfive/python/trunk/lib@8630 07aa3797-eaea-0310-9497-ec20f46c85ba

--- utils/dates.py
+++ utils/dates.py
@@ -8,8 +8,3 @@ MONTHS = {
     9:_('Sep (09)'), 10:_('Oct (10)'), 11:_('Nov (11)'), 12:_('Dec (12)')
 }
 
-MONTHS_LONG = {
-    1:_('January'), 2:_('February'), 3:_('March'), 4:_('April'),
-    5:_('May'), 6:_('June'), 7:_('July'), 8:_('August'),
-    9:_('September'), 10:_('October'), 11:_('November'), 12:_('December')
-}
