e6a7566b5abc9820cdcbebee50d7b6cdcf04c656
ravasthi <ravasthi@55minutes.com>
2009-04-29 22:00:20 +0000

    Add new enumeration for full month names.
    git-svn-id: file:///opt/custom/var/svn/repos/internal/fiftyfive/python/trunk/lib@8629 07aa3797-eaea-0310-9497-ec20f46c85ba

--- utils/dates.py
+++ utils/dates.py
@@ -7,3 +7,9 @@ MONTHS = {
     5:_('May (05)'), 6:_('Jun (06)'), 7:_('Jul (07)'), 8:_('Aug (08)'),
     9:_('Sep (09)'), 10:_('Oct (10)'), 11:_('Nov (11)'), 12:_('Dec (12)')
 }
+
+MONTHS_LONG = {
+    1:_('January'), 2:_('February'), 3:_('March'), 4:_('April'),
+    5:_('May'), 6:_('June'), 7:_('July'), 8:_('August'),
+    9:_('September'), 10:_('October'), 11:_('November'), 12:_('December')
+}
