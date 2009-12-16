2c2d2fac8415984fdcdfcf59073a9c988e5b2b5b
gsong <george@55minutes.com>
2009-12-16 06:06:08 +0000

    Can't get the decorator to work properly.
    git-svn-id: file:///opt/custom/var/svn/repos/internal/fiftyfive/python/trunk/lib@8857 07aa3797-eaea-0310-9497-ec20f46c85ba

--- src/fiftyfive/fabric/decorators.py
+++
@@ -1,9 +0,0 @@
-def command(func):
-    """
-    A Fabric command decorator which registers it as a valid command.
-    Uses the __all__ Python facility within a module.
-    Make sure to declare `__all__ = []` at the top of your source code.
-    """
-    global __all__
-    __all__.append(func.__name__)
-    return func
