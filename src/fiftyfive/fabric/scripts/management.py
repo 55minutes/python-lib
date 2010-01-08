6594f5ac1ad7d880d4abf191c6fdd630332afc59
gsong <george@55minutes.com>
2010-01-08 06:24:53 +0000

    First pass at refactoring some common Fabric Django management commands.
    git-svn-id: file:///opt/custom/var/svn/repos/internal/fiftyfive/python/trunk/lib@8884 07aa3797-eaea-0310-9497-ec20f46c85ba

---
+++ src/fiftyfive/fabric/scripts/management.py
@@ -0,0 +1,84 @@
+from functools import wraps
+import inspect
+import os
+
+from fabric.api import *
+from fiftyfive.fabric.api import *
+from fiftyfive.fabric.scripts.deploy import _ve_run
+
+__all__ = []
+
+
+env._apps_data = 'sample_data.json'
+env._fixtures = [env._apps_data]
+
+
+def command(func):
+    global __all__
+    __all__.append(func.__name__)
+    return func
+
+
+def set_django_environment(settings_module):
+    os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
+    from django.conf import settings as django_settings
+    env._settings = django_settings
+    env._fixture_dir = django_settings.FIXTURE_DIRS[0]
+    env._apps_fixture = os.path.join(env._fixture_dir, env._apps_data)
+    env._database_engine = django_settings.DATABASE_ENGINE
+
+
+def _local_or_run(cmd, capture=False):
+    if env.host:
+        caller = inspect.stack()[1][3]
+        _ve_run('projectfab %(caller)s' % vars())
+    else:
+        local(cmd, capture)
+
+
+@command
+def graph_models():
+    apps = env._apps
+    outfile = env._model_graph_file
+    local('manage graph_models %(apps)s -g -o %(outfile)s' % vars()) 
+
+
+@command
+def echo_fixturedir():
+    print env._fixture_dir
+
+
+@command
+def dump_apps_data():
+    apps = env._apps
+    fixture = env._apps_fixture
+    _local_or_run('manage dumpdata --indent=2 %(apps)s > %(fixture)s' % vars())
+
+
+def create_update_with_remote_fixtures(dumpdata_func):
+    def decorator(func):
+        @wraps(func)
+        def update_func():
+            if len(env.hosts) != 1:
+                abort("Requires one and *only* one remote host.")
+            if not env._is_production:
+                cont = prompt(
+                    'Update fixtures from non-production environment? (Y/n)',
+                    default='n', validate=r'[Y|n]')
+                cont = (cont=='Y' and True) or (cont=='n' and False)
+            else:
+                cont = True
+            if not cont:
+                abort('Updating of fixtures aborted.')
+            dumpdata_func()
+            remote_dir = _ve_run(
+                'projectfab echo_fixturedir').split(os.linesep)[0].strip()
+            for f in env._fixtures:
+                remote_sample = os.path.join(remote_dir, f)
+                run('bzip2 -f %(remote_sample)s' % vars())
+                get('%(remote_sample)s.bz2' % vars(), env._fixture_dir)
+                run('bunzip2 -f %(remote_sample)s.bz2' % vars())
+                local_sample = os.path.join(env._fixture_dir, f)
+                local('bunzip2 -f %(local_sample)s.bz2' % vars())
+        return update_func
+    return decorator
---
+++ src/fiftyfive/fabric/scripts/management_extras.py
@@ -0,0 +1,44 @@
+from fabric.api import *
+from fiftyfive.fabric.api import *
+
+__all__ = []
+
+
+def command(func):
+    global __all__
+    __all__.append(func.__name__)
+    return func
+
+
+@command
+def loaddata():
+    fixture = env._apps_fixture
+    _local_or_run('manage loaddata %(fixture)s' % vars())
+
+
+@command
+def syncdb():
+    _local_or_run('manage syncdb --noinput')
+
+
+@command
+def initdata():
+    if env.host:
+        run('createdb -w %s' % env._dbname)
+    syncdb()
+    loaddata()
+
+
+@command
+def dropdb():
+    if env.host:
+        run('dropdb -w %s' % env._dbname)
+    else:
+        local('rm %s' %env._settings.DATABASE_NAME)
+
+
+@command
+def resetdb():
+    with settings(warn_only=True):
+        dropdb()
+    initdata()
