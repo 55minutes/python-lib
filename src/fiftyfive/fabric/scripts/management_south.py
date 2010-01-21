d8a40e8611e465a53858ac43614451beb433981d
gsong <george@55minutes.com>
2010-01-21 07:06:25 +0000

    Adding South management commands.
    git-svn-id: file:///opt/custom/var/svn/repos/internal/fiftyfive/python/trunk/lib@8900 07aa3797-eaea-0310-9497-ec20f46c85ba

---
+++ src/fiftyfive/fabric/scripts/management_south.py
@@ -0,0 +1,39 @@
+from fabric.api import *
+from fiftyfive.fabric.api import *
+
+from management import _local_or_run
+from management_extras import *
+
+__all__ = []
+
+
+def command(func):
+    global __all__
+    __all__.append(func.__name__)
+    return func
+
+
+@command
+def resetdb():
+    with settings(warn_only=True):
+        dropdb()
+    initdata()
+
+
+@command
+def initdata():
+    if env.host:
+        run('createdb -w %s' % env._dbname)
+    syncdb()
+    migrate()
+    loaddata()
+
+
+@command
+def south_init():
+    _local_or_run('manage syncdb && manage migrate 0001 --all --fake')
+
+
+@command
+def migrate():
+    _local_or_run('manage migrate --all')
